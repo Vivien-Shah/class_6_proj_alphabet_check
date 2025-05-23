e19fdff7da201b3a13aba7e4a7e8cb141407629b
Vivien-Shah <hfsthane7787@gmail.com>
2025-05-23 19:46:12 +0530

    Add files via upload

---
+++ class6_proj_alphabet check.py
@@ -0,0 +1,11 @@
+# Program to check if character is an alphabet.
+char=input("Enter a character: ")
+if char >= 'a' and char <= 'z':
+    print("This character is an alphabet")
+
+elif char >= 'A' and char <= 'Z':
+    print("This character is an alphabet")
+
+else:  
+    print("This character is not an alphabet")
+
